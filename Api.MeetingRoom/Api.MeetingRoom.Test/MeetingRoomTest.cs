d469900ff5e7f808b273b59045684d5764035f6d
lucas <lucasrodriguesmaximiano1@gmail.com>
2019-10-23 23:37:24 -0300

    FTE-001 estrutura do projeto e teste de integração

---
+++ Api.MeetingRoom/Api.MeetingRoom.Test/MeetingRoomTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Api.MeetingRoom.Test
+{
+    [TestFixture]
+    public class MeetingRoomTest
+    {
+
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public MeetingRoomTest()
+        {
+            // Arrange
+            _server = new TestServer(new WebHostBuilder()
+               .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+       
+        [Test]
+        [TestCase(1, 10)]
+        [TestCase(2, 10)]
+        public async Task GetMusicRecordByFilterWithSucess(int page, int pageSize)
+        {
+            try
+            {
+                var response = await _client.GetAsync($"/meetingroom?page={page}&pageSize={pageSize}");
+
+                Assert.IsNotNull(response.Content);
+
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
